1f17d2502f5d01d7ca56a1e461a30a69e70139ad
Ellie Kallmier <e.kallmier@student.unsw.edu.au>
2023-03-07 16:37:26 +1100

    added emissions placeholder and updated formatting

--- scenario_runner.py
+++ scenario_runner.py
@@ -10,19 +10,39 @@ import numpy as np
 # --> idea: add 'Hybrids' input: list of gen ids to hybridise w/ percentages and use Generator ID as the name for new hybrid.
 
 
-def run_scenario_from_row(scenario_row, price_profiles, load_profiles, charge_set):
+def run_scenario_from_row(scenario_row, price_profiles, load_profiles, emissions_profiles, charge_set):
     """
     Calculate retail and ppa costs for a given row in the scenario table
 
+    :param scenario_row: pandas DataFrame row with following columns 
+              'PPA', 
+              'PPA_Volume',
+              'Wholesale_Exposure_Volume',
+              'PPA_Price', 
+              'Excess_RE_Purchase_Price',
+              'Excess_RE_Sale_Price', 
+              'LGC_Volume_Type', 
+              'LGC_Purhcase_Volume',
+              'LGC_Purchase_Price', 
+              'Load_MLF', 
+              'Load_DLF', 
+              'Generator_MLF',
+              'Generator_DLF', 
+              'Target_Period', 
+              'Yearly_Target_MWh', 
+              'Yearly_Short_Fall_Penalty_MWh',
+              'Yearly_LGC_target_LGC', 
+              'Yearly_LGC_short_fall_penalty_LGC', 
+              'Average_Wholesale_Price',
+              'Wholesale_Price_ID',
+              'Load_ID', 
+              'Generator_ID', 
+              'Emissions Region ID', 
+              'TOU_1', 'TOU_2', 'TOU_3', 'TOU_4', 'TOU_5', 'TOU_6', 'TOU_7', 'TOU_8', 'TOU_9', 'TOU_10',
+              'Flat_1', 'Flat_2', 'Flat_3', 'Flat_4', 'Flat_5', 'Flat_6', 'Flat_7', 'Flat_8'
     :param price_profiles:
-    :param scenario_row: pandas DataFrame row with following columns 'PPA', 'Wholesale_Price_ID',
-           'Average_Wholesale_Price', 'Wholesale_Exposure_Volume', 'Load_ID', 'Generator_ID', 'PPA_Volume',
-           'PPA_Price', 'Excess_RE_Purchase_Price', 'Excess_RE_Sale_Price', 'LGC_Volume_Type', 'LGC_Purhcase_Volume',
-           'LGC_Purchase_Price', 'Yearly_Target_MWh', 'Target_Period', 'Yearly_Short_Fall_Penalty_MWh',
-           'Yearly_LGC_target_LGC', 'Yearly_LGC_short_fall_penalty_LGC', 'Load_MLF', 'Load_DLF', 'Generator_MLF',
-           'Generator_DLF', 'TOU_1', 'TOU_2', 'TOU_3', 'TOU_4', 'TOU_5', 'TOU_6', 'TOU_7', 'TOU_8', 'TOU_9', 'TOU_10',
-           'Flat_1', 'Flat_2', 'Flat_3', 'Flat_4', 'Flat_5', 'Flat_6', 'Flat_7', 'Flat_8'
     :param load_profiles: The set of possible load profiles
+    :param emissions_profiles: The set of possible emissions profiles
     :param charge_set: The set of retail charge details
     :return: retail_cost: float
     """
@@ -30,6 +50,7 @@ def run_scenario_from_row(scenario_row, price_profiles, load_profiles, charge_se
     load_id = scenario_row['Load_ID']
     generator_id = scenario_row['Generator_ID']
     price_id = scenario_row['Wholesale_Price_ID']
+    emissions_id = scenario_row['Emissions Region ID']
 
     load_profiles['DateTime'] = pd.to_datetime(load_profiles["DateTime"], format="%d/%m/%Y %H:%M")
     load_profiles[load_id] = pd.to_numeric(load_profiles[load_id])
@@ -37,6 +58,9 @@ def run_scenario_from_row(scenario_row, price_profiles, load_profiles, charge_se
 
     price_profiles['DateTime'] = pd.to_datetime(price_profiles["DateTime"])
     price_profiles[price_id] = pd.to_numeric(price_profiles[price_id])
+
+    emissions_profiles['DateTime'] = pd.to_datetime(emissions_profiles["DateTime"])
+    emissions_profiles[emissions_id] = pd.to_numeric(emissions_profiles[emissions_id])
 
     residual_profiles = residuals.calc(load_profiles=load_profiles, load_id=load_id, generator_id=generator_id)
     retail_costs = tariffs.calc_tou_set(tou_set=charge_set, load_profiles=residual_profiles, contract_type=scenario_row['PPA'],
