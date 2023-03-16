c5161e6e104cfbb6ee78c30bc629a2904dd423b2
Ellie Kallmier <e.kallmier@student.unsw.edu.au>
2023-03-16 17:11:22 +1100

    added emissions col

--- residuals.py
+++ residuals.py
@@ -10,7 +10,7 @@ def calc(load_profiles, load_id, generator_id):
     :param load_profiles: pandas dataframe, the full set of input profiles defined, only fixed column name is DateTime
     :param load_id: string, the name of the column in load_profiles to treat as the load
     :param generator_id: string, the name of the column in load_profiles to treat as the RE generator
-    :return: residual_profiles: pandas dataframe, with column DateTime, Load, RE Generator, Black, Excess RE, Used RE
+    :return: residual_profiles: pandas dataframe, with column DateTime, Load, RE Generator, Black, Excess RE, Used RE, Empty, Average Emissions Intensity
     """
     residual_profiles = pd.DataFrame()
     residual_profiles['DateTime'] = load_profiles['DateTime']
@@ -20,5 +20,5 @@ def calc(load_profiles, load_id, generator_id):
     residual_profiles['Excess RE'] = np.maximum(residual_profiles['RE Generator'] - residual_profiles['Load'], 0)
     residual_profiles['Used RE'] = residual_profiles['RE Generator'] - residual_profiles['Excess RE']
     residual_profiles['Empty'] = 0
-    residual_profiles['Average Emissions Intensity'] = load_profiles['Average Emissions Intensity']   # update this
+    residual_profiles['Average Emissions Intensity'] = load_profiles['Average Emissions Intensity']  
     return residual_profiles
