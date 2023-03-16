61a185ebb066a2c1976e47023716e20dc5290d42
Ellie Kallmier <e.kallmier@student.unsw.edu.au>
2023-03-16 17:11:42 +1100

    fixed formatting (indents)

--- scenario_runner.py
+++ scenario_runner.py
@@ -12,61 +12,64 @@ import numpy as np
 
 
 def run_scenario_from_row(scenario_row, price_profiles, load_profiles, charge_set, emissions_profiles):
-    """
-    Calculate retail and ppa costs for a given row in the scenario table
+   """
+   Calculate retail and ppa costs for a given row in the scenario table
 
-    :param scenario_row: pandas DataFrame row with following columns 
-       'Scenario_ID'
-       'PPA', 
-       'PPA_Volume',
-       'Wholesale_Exposure_Volume',
-       'PPA_Price', 
-       'Excess_RE_Purchase_Price',
-       'Excess_RE_Sale_Price', 
-       'LGC_Volume_Type', 
-       'LGC_Purhcase_Volume',
-       'LGC_Purchase_Price', 
-       'Load_MLF', 
-       'Load_DLF', 
-       'Generator_MLF',
-       'Generator_DLF', 
-       'Target_Period', 
-       'Yearly_Target_MWh', 
-       'Yearly_Short_Fall_Penalty_MWh',
-       'Yearly_LGC_target_LGC', 
-       'Yearly_LGC_short_fall_penalty_LGC', 
-       'Average_Wholesale_Price',
-       'Wholesale_Price_ID',
-       'Load_ID', 
-       'Generator_ID', 
-       'Emissions_Region_ID', 
-       'TOU_1', 'TOU_2', 'TOU_3', 'TOU_4', 'TOU_5', 'TOU_6', 'TOU_7', 'TOU_8', 'TOU_9', 'TOU_10',
-       'Flat_1', 'Flat_2', 'Flat_3', 'Flat_4', 'Flat_5', 'Flat_6', 'Flat_7', 'Flat_8'
-    :param price_profiles:
-    :param load_profiles: The set of possible load profiles
-    :param emissions_profiles: The set of possible emissions profiles
-    :param charge_set: The set of retail charge details
-    :return: retail_cost, ppa_cost: float
-    """
+   :param scenario_row: pandas DataFrame row with following columns 
+      'Scenario_ID'
+      'PPA', 
+      'PPA_Volume',
+      'Wholesale_Exposure_Volume',
+      'PPA_Price', 
+      'Excess_RE_Purchase_Price',
+      'Excess_RE_Sale_Price', 
+      'LGC_Volume_Type', 
+      'LGC_Purhcase_Volume',
+      'LGC_Purchase_Price', 
+      'Load_MLF', 
+      'Load_DLF', 
+      'Generator_MLF',
+      'Generator_DLF', 
+      'Target_Period', 
+      'Yearly_Target_MWh', 
+      'Yearly_Short_Fall_Penalty_MWh',
+      'Yearly_LGC_target_LGC', 
+      'Yearly_LGC_short_fall_penalty_LGC', 
+      'Average_Wholesale_Price',
+      'Wholesale_Price_ID',
+      'Load_ID', 
+      'Generator_ID', 
+      'Emissions_Region_ID', 
+      'TOU_1', 'TOU_2', 'TOU_3', 'TOU_4', 'TOU_5', 'TOU_6', 'TOU_7', 'TOU_8', 'TOU_9', 'TOU_10',
+      'Flat_1', 'Flat_2', 'Flat_3', 'Flat_4', 'Flat_5', 'Flat_6', 'Flat_7', 'Flat_8'
+   :param price_profiles:
+   :param load_profiles: The set of possible load profiles
+   :param emissions_profiles: The set of possible emissions profiles
+   :param charge_set: The set of retail charge details
+   :return: retail_cost, ppa_cost: float
+   """
 
-    load_id = scenario_row['Load_ID']
-    generator_id = scenario_row['Generator_ID']
-    price_id = scenario_row['Wholesale_Price_ID']
-    emissions_id = scenario_row['Emissions_Region_ID']
+   load_id = scenario_row['Load_ID']
+   generator_id = scenario_row['Generator_ID']
+   price_id = scenario_row['Wholesale_Price_ID']
+   emissions_id = scenario_row['Emissions_Region_ID']
 
-    load_profiles['DateTime'] = pd.to_datetime(load_profiles["DateTime"])#, format="%d/%m/%Y %H:%M")
-    load_profiles[load_id] = pd.to_numeric(load_profiles[load_id])
-    load_profiles[generator_id] = pd.to_numeric(load_profiles[generator_id])
-    load_profiles['Average Emissions Intensity'] = pd.to_numeric(emissions_profiles[emissions_id])
+   load_profiles['DateTime'] = pd.to_datetime(load_profiles["DateTime"])#, format="%d/%m/%Y %H:%M")
+   load_profiles[load_id] = pd.to_numeric(load_profiles[load_id])
 
-    price_profiles['DateTime'] = pd.to_datetime(price_profiles["DateTime"])
-    price_profiles[price_id] = pd.to_numeric(price_profiles[price_id])
+   # Add hybrid function here if flagged
+   load_profiles[generator_id] = pd.to_numeric(load_profiles[generator_id])
+   load_profiles['Average Emissions Intensity'] = pd.to_numeric(emissions_profiles[emissions_id])
 
-    residual_profiles = residuals.calc(load_profiles=load_profiles, load_id=load_id, generator_id=generator_id)
-    retail_costs = tariffs.calc_tou_set(tou_set=charge_set, load_profiles=residual_profiles, contract_type=scenario_row['PPA'],
-                                        wholesale_volume=scenario_row['Wholesale_Exposure_Volume'])
-    retail_cost = retail_costs['Cost'].sum()
-    ppa_cost = ppa.calc_by_row(scenario_row, price_profiles[price_id], residual_profiles)
-    firming_emissions = emissions.firming_emissions_calc(residual_profiles)
+   price_profiles['DateTime'] = pd.to_datetime(price_profiles["DateTime"])
+   price_profiles[price_id] = pd.to_numeric(price_profiles[price_id])
 
-    return retail_cost, ppa_cost, firming_emissions
+   # Add in the scaling function here
+   residual_profiles = residuals.calc(load_profiles=load_profiles, load_id=load_id, generator_id=generator_id)
+   retail_costs = tariffs.calc_tou_set(tou_set=charge_set, load_profiles=residual_profiles, contract_type=scenario_row['PPA'],
+                                       wholesale_volume=scenario_row['Wholesale_Exposure_Volume'])
+   retail_cost = retail_costs['Cost'].sum()
+   ppa_cost = ppa.calc_by_row(scenario_row, price_profiles[price_id], residual_profiles)
+   firming_emissions = emissions.firming_emissions_calc(residual_profiles)
+
+   return retail_cost, ppa_cost, firming_emissions
