bd763ae96451c0ebe39b0af69a2ea10e73992252
Ellie Kallmier <e.kallmier@student.unsw.edu.au>
2023-06-28 10:11:07 +1000

    setup blank charts file

---
+++ charts.py
@@ -0,0 +1,16 @@
+# File for creating useful charts
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Pricing charts
+
+
+# Emissions comparisons
+
+
+# Profile plots
+
+
+# Financial outcomes 
