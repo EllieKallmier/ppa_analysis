699ac111e5912863192555045cef61a5a5da372e
EllieKallmier <61219730+EllieKallmier@users.noreply.github.com>
2024-06-12 15:37:57 +1000

    added battery functionality

--- ppa_analysis/user_inputs.py
+++ ppa_analysis/user_inputs.py
@@ -361,3 +361,39 @@ def launch_generator_data_editor(input_collector):
         names='value'
     )
     return generator_data_editor
+
+
+def launch_battery_input_collector():
+
+    display(HTML(
+        '''
+        <style>
+        .widget-label { min-width: 30ex !important; }
+        .widget-select select { min-width: 70ex !important; }
+        .widget-dropdown select { min-width: 70ex !important; }
+        .widget-floattext input { min-width: 70ex !important; }
+        </style>
+        '''
+    ))
+
+    display(HTML(
+        '''
+        <h3>Battery inputs:</h3>
+        '''
+    ))
+
+    battery_input_collector = {}
+
+    battery_input_collector['rated_power_capacity'] = widgets.FloatText(
+        value=1.0,
+        description='Rated power capacity (MW):',
+    )
+    display(battery_input_collector['rated_power_capacity'])
+
+    battery_input_collector['size_in_mwh'] = widgets.FloatText(
+        value=2.0,
+        description='Battery size (MWh):',
+    )
+    display(battery_input_collector['size_in_mwh'])
+
+    return battery_input_collector
